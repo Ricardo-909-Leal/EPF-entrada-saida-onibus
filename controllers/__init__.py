70a57c3744aa330a255b9bf7e8696d088694f6dd
Ricardo-909-Leal <ricardoeguiff@email.com>
2025-07-01 16:09:03 -0300

    adicao da rotas fiscal e terminal

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,9 +1,15 @@
 from bottle import Bottle
 from controllers.user_controller import user_routes
 from controllers.motorista_controller import motorista_routes
-from controllers.onibus_controller import onibus_routes 
+from controllers.onibus_controller import onibus_routes
+from controllers.fiscal_controller import fiscal_routes
+from controllers.terminal_controller import terminal_routes
+
+
 
 def init_controllers(app: Bottle):
     app.merge(user_routes)
     app.merge(motorista_routes)
     app.merge(onibus_routes)
+    app.merge(fiscal_routes)
+    app.merge(terminal_routes)
